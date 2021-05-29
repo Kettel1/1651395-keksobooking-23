7c76d09b763729f3184b122c69fc27f9f17bd7a0
Kettel <75881317+Kettel1@users.noreply.github.com>
2021-05-29 20:43:39 +0300

    Сделал стрелочные функции

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-getRandomIntegerNumber();
-getRandomFractionalNumber();
-
-function getRandomIntegerNumber(firstNumber, secondNumber) {
+const getRandomIntegerNumber = (firstNumber, secondNumber) => {
   firstNumber = Math.ceil(firstNumber);
   secondNumber = Math.floor(secondNumber);
 
   if (Math.sign(firstNumber) === -1 || Math.sign(secondNumber) === -1) {
-    alert('Не положительные числа');
+    alert ('Не положительные числа');
   } else {
-    return alert(Math.floor(Math.random() * (secondNumber - firstNumber) + firstNumber));
+    return alert (Math.floor(Math.random() * (secondNumber - firstNumber) + firstNumber));
   }
-}
+};
 
-function getRandomFractionalNumber(firstNumber, secondNumber, quantitySign) {
-  let result = Math.random() * (secondNumber - firstNumber) + firstNumber;
+const getRandomFractionalNumber = (firstNumber, secondNumber, quantitySign) => {
+  const result = Math.random() * (secondNumber - firstNumber) + firstNumber;
   alert(result.toFixed(quantitySign));
 };
 
-
+getRandomIntegerNumber();
+getRandomFractionalNumber();
