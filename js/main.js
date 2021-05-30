360cdb4942226ab7c40cd3a1e5c14686df4bfe38
Kettel <75881317+Kettel1@users.noreply.github.com>
2021-05-30 18:38:35 +0300

    Убрал алерты

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ const getRandomIntegerNumber = (firstNumber, secondNumber) => {
   if (Math.sign(firstNumber) === -1 || Math.sign(secondNumber) === -1) {
     alert ('Не положительные числа');
   } else {
-    return alert (Math.floor(Math.random() * (secondNumber - firstNumber) + firstNumber));
+    return (Math.floor(Math.random() * (secondNumber - firstNumber) + firstNumber));
   }
 };
 
 const getRandomFractionalNumber = (firstNumber, secondNumber, quantitySign) => {
   const result = Math.random() * (secondNumber - firstNumber) + firstNumber;
-  alert(result.toFixed(quantitySign));
+  return result.toFixed(quantitySign);
 };
 
-getRandomIntegerNumber();
+getRandomIntegerNumber(1, 30);
 getRandomFractionalNumber();
