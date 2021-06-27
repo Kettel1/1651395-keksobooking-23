ace6133d81483250d0def3af3f1242ed0fa10be8
Kettel <75881317+Kettel1@users.noreply.github.com>
2021-06-27 21:07:56 +0300

    Правда или действие 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {generateOffer} from './form.js';
 import {createOffer} from './data.js';
 import {enableForm, disabledForm} from './toggle.js';
+import {title, price} from './validation.js';
 
 const SIMILAR_OFFER_COUNT = 10;
 const similarOffer = new Array(SIMILAR_OFFER_COUNT).fill().map(createOffer);
@@ -9,5 +10,10 @@ const allOffersFragment = generateOffer(similarOffer);
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(allOffersFragment.firstChild);
 
-// enableForm();
-// disabledForm();
+
+// Тонкости работы нашего линтера :)
+enableForm();
+disabledForm();
+
+title;
+price;
